bb2620ebad1d9db54942bd4479b60127c1062c70
Boris Starkov <boris.starkow@gmail.com>
2021-05-23 19:43:21 +0300

    implement logic

--- src/Field.tsx
+++ src/Field.tsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {List} from 'immutable';
+
 import Tile from './Tile';
 
-interface Props {}
+interface Coord {
+    x: number,
+    y: number
+}
 
-const Field = (props : Props) => {
-    const field = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0, 15];
+const getxy = (ind : number) : Coord => {
+    const x = ind % 4;
+    const y = (ind - x) / 4;
+    return {'x': x, 'y': y};
+}
 
-    const tiles = field.map(x => <Tile value={x} key={x}/>)
+const adjacent = (a: Coord, b: Coord) : boolean => {
+    return (
+        (a.x === b.x && Math.abs(a.y - b.y) === 1)
+        || (a.y === b.y && Math.abs(a.x - b.x) === 1)
+    );
+}
 
-    return (<div className='field'>
-        {tiles}
+const swap = (list : List<number>, pos1 : number, pos2: number) : List<number> => {
+    const tmp = list.get(pos1);
+    return list.set(pos1, list.get(pos2) as number).set(pos2, tmp as number);
+}
+
+const Field = () => {
+    const [field, setField] = useState(List([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0, 15]));
+    const [clicksCnt, setClicksCnt] = useState(0);
+
+    const handleClick = (val: number) => {
+        if (val === 0) return;
+        const curTileCoord = getxy(field.indexOf(val));
+        const emptyTileCoord = getxy(field.indexOf(0));
+        if (adjacent(curTileCoord, emptyTileCoord)) {
+            setField(swap(field, field.indexOf(val), field.indexOf(0)));
+            setClicksCnt(clicksCnt + 1);
+        }
+    }
+
+    const tiles = field.map(x => <Tile value={x} clickHandler={() => handleClick(x)} key={x}/>)
+
+    return (<div>
+                <h5>
+                    Clicks: {clicksCnt}
+                </h5>
+                <div className='field'>
+                    {tiles}
+                </div>
     </div>);
 }
 
--- src/Tile.tsx
+++ src/Tile.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './Field.css';
 
 interface Props {
-    value: number | null
+    value: number | null,
+    clickHandler(): void
 }
 
 const Tile = (props : Props) => {
     let cls = 'tile';
     if (props.value === 0) cls += ' empty';
     return (
-        <div className={cls}>
+        <div className={cls} onClick={props.clickHandler}>
             <span className='content'>{props.value}</span>
         </div>
     )
